61667c38a89ddeee4c858a27a54f79f13450e66a
lim44104 <s10207455@connect.np.edu.sg>
2021-05-15 16:40:44 +0800

    Complete Practical 4

--- app/src/main/java/sg/edu/np/mad/madpractical/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/ListActivity.java
@@ -1,27 +1,40 @@
 package sg.edu.np.mad.madpractical;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
     private final static String TAG = "List Activity";
+    private final ArrayList<User> userList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        for (int i = 0; i < 20; ++i) {
+            userList.add(new User(
+                    "Name" + rng(), "Description " + rng(), i, rngFollow()
+            ));
+        }
         Log.v(TAG, "List Activity Created");
 
+        RecyclerView recyclerViewCustom = findViewById(R.id.recyclerView);
+        CustomAdapter cAdapter = new CustomAdapter(userList, ListActivity.this);
+        LinearLayoutManager cLayoutManager = new LinearLayoutManager(this);
+        recyclerViewCustom.setLayoutManager(cLayoutManager);
+        recyclerViewCustom.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewCustom.setAdapter(cAdapter);
+
+        /*
         ImageView alertImg = findViewById(R.id.round_android);
         alertImg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +58,15 @@ public class ListActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+
+         */
+    }
+
+    private boolean rngFollow() {
+        return new Random().nextInt(2) == 1;
+    }
+
+    private int rng() {
+        return new Random().nextInt();
     }
 }
--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -2,7 +2,7 @@ package sg.edu.np.mad.madpractical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,10 +10,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import sg.edu.np.mad.madpractical.R;
+//import sg.edu.np.mad.madpractical.R;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "Main Activity";
+    private boolean initialFollowed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,36 +36,46 @@ public class MainActivity extends AppCompatActivity {
 
         Button followButton = findViewById(R.id.followButton);  //define follow button
 
+        Bundle bundle = getIntent().getExtras();
 
+        if (bundle != null) {
+            user = bundle.getParcelable("user");
+            initialFollowed = user.isFollowed();
+            userName.setText(user.name);
+            userDescription.setText(user.description);
+            followButton.setText(user.isFollowed() ? "Unfollow" : "Follow");
+        }
+        else {
+            user = new User("hi", "hi", 1, false);
+        }
 
-        followButton.setOnClickListener(new View.OnClickListener() {    //button event listener
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG, "Button Pressed!");
+        User finalUser = user;
+        //button event listener
+        followButton.setOnClickListener(v -> {
+            Log.v(TAG, "Button Pressed!");
 
-                if (user.isFollowed()) {    //if false (haven't follow), set to false and set text to Follow
-                    user.setFollowed(false);
-                    followButton.setText("Follow");
-                    Log.v(TAG, "User is not Followed.");
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(
-                            MainActivity.this,
-                            "Unfollowed",
-                            duration);
-                    toast.show();
-                }
+            if (finalUser.isFollowed()) {    //if false (haven't follow), set to false and set text to Follow
+                finalUser.setFollowed(false);
+                followButton.setText("Follow");
+                Log.v(TAG, "User is not Followed.");
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(
+                        MainActivity.this,
+                        "Unfollowed",
+                        duration);
+                toast.show();
+            }
 
-                else {
-                    user.setFollowed(true); //if true (followed), set to true and set text to Followed
-                    followButton.setText("Followed");
-                    Log.v(TAG, "User is Followed.");
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(
-                            MainActivity.this,
-                            "Followed",
-                            duration);
-                    toast.show();
-                }
+            else {
+                finalUser.setFollowed(true); //if true (followed), set to true and set text to Followed
+                followButton.setText("Followed");
+                Log.v(TAG, "User is Followed.");
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(
+                        MainActivity.this,
+                        "Followed",
+                        duration);
+                toast.show();
             }
         });
 
